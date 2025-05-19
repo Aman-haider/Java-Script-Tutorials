5aabe2d37dc662445f0e2ffa7b8236503cbb5424
Aman-Haider <amankacho0001@gmail.com>
2025-05-19 13:54:18 +0000

    add one file for loop

--- 01_basic/01_test.js
+++ 01_basic/01_test.js
@@ -9,3 +9,12 @@ let stringInNunber = Number( number)
 console.log(typeof stringInNunber);
 
 console.log(1 + 3+"22");
+
+
+for (let step = 0; step < 10; step++) {
+  // Runs 5 times, with values of step 0 through 4.
+  console.log("Hello World!");
+}
+
+
+
