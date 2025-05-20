1900e97ef02e7d4323ae44e9bb7345f5701cb6ba
Aman-Haider <amankacho0001@gmail.com>
2025-05-20 14:15:18 +0000

    complete study string

---
+++ 01_basic/03_string.js
@@ -0,0 +1,15 @@
+
+const gameName = "clan of clash"
+
+const userName = "  Aman"
+ 
+console.log( ` ${gameName} and my name is ${userName}`  );
+
+console.log(userName.toUpperCase);
+
+const anotherName = new String("Haider")
+
+console.log( anotherName.toUpperCase);
+
+
+console.log(gameName.charAt (3));
