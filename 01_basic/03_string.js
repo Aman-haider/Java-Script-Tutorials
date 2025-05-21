88bfeb5783ec2867b4e36f8641415fa61c859633
Aman-Haider <amankacho0001@gmail.com>
2025-05-21 07:27:59 +0000

    methods of String

--- 01_basic/03_string.js
+++ 01_basic/03_string.js
@@ -1,5 +1,5 @@
 
-const gameName = "clan of clash"
+const gameName = "clan_of_ clash"
 
 const userName = "  Aman"
  
@@ -9,7 +9,38 @@ console.log(userName.toUpperCase);
 
 const anotherName = new String("Haider")
 
-console.log( anotherName.toUpperCase);
+console.log( anotherName.toUpperCase() );
 
 
-console.log(gameName.charAt (3));
+console.log(anotherName.charAt(4));
+
+console.log(anotherName.indexOf('d'));
+
+const newString = anotherName.substring(0, 4)
+
+console.log(newString);
+
+const anotherString = anotherName.slice(-6, 4)
+
+console.log(anotherString);
+
+const newStringOne = "   Aman Balghari     "
+
+console.log(newStringOne);
+
+const url = "https:\\facebook.com.aman%20haider"
+
+console.log(url.replace( '%20'   ,   '_'  ));
+
+console.log(url.includes('aman'));
+
+console.log(url.includes('balghari'));
+
+console.log(gameName.split('clan'))
+
+console.log(gameName.);
+
+
+
+
+
