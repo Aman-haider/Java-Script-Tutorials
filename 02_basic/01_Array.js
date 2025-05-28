00a9ff57d5a2f03535c93aa94b1b7ed15ea2e3f0
Aman-Haider <amankacho0001@gmail.com>
2025-05-28 10:33:47 +0000

    Array Methods

--- 02_basic/01_Array.js
+++ 02_basic/01_Array.js
@@ -3,5 +3,31 @@
 
 
 
-const myArr = [ 0,1, 2, 3 ,4,4,5,6,]
-console.log(myArr);
+const myArr = [ 0,1, 2, 3 ,4,5,6 ]
+// console.log(myArr);
+
+
+console.log( myArr.indexOf(3));
+// console.log(myArr);
+
+
+console.log("A" , myArr);
+
+const newArr1 = myArr.slice(1,4)
+
+console.log(newArr1);
+
+console.log("B" , myArr);
+
+const newArr = myArr.splice(1,4)
+
+console.log(newArr);
+
+console.log("C" , myArr);
+
+
+
+
+
+
+
