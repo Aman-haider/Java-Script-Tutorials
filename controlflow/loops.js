2e2b80169a8eef1a30609ebd60811614a94e5765
Aman-Haider <amankacho0001@gmail.com>
2025-06-11 10:43:47 +0000

    for loop

---
+++ controlflow/loops.js
@@ -0,0 +1,84 @@
+//////Loops/////
+
+
+/** for (let i = 0; i <= 10; i++) {
+
+    if (i == 5) {
+        // console.log("5 is the best");
+        
+        
+    }
+    // console.log(`value of i is ${i}`);
+    
+    
+} */
+
+/**for (let i = 10; i >=0; i--) {
+    console.log(`outer loop ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`inner loop ${j} innerloop ${i}`);
+        
+        
+    }
+ 
+}
+for (let i = 0; i <= 10; i++) {
+    
+    for (let j = 0; j <= 10; j++) {
+        // console.log(i + '*' + j + '=' + i *  j );
+    
+        
+    }
+ 
+}
+for (let i = 0; i <= 10; i++) {
+    
+    for (let j = 0; j <= 10; j++) {
+        
+        // console.log(`${i} * ${j} = ${i * j} `);
+        
+    
+        
+    }
+ 
+}*/
+
+// for (let i= 0; i=5 ; i--){
+//     console.log(i)
+// }
+for (let i = 2; i <=100; i++) {
+    if(i % 2 == 0){
+    //  console.log(i);
+    } 
+}
+    for (let i = 1; i <=100; i+=2) {
+        
+        // console.log(i);
+        
+    }
+
+    for (let i = 0; i <= 100; i++) {
+        let isPrime = true;
+        for (let j = 0; j <Math.sqrt(i); j++) {
+            
+            if (i% j === 0) {
+                console.log(j);
+                
+            }
+            
+        }
+        // if (isPrime) { 
+        //     console.log(i);
+        
+            
+        // }
+        
+    }
+
+ 
+
+
+    
+ 
+
+
