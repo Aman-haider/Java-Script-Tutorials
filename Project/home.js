0564dc0bca66b88896bec3f59595f8fc3aea37db
Aman-Haider <amankacho0001@gmail.com>
2025-06-29 18:07:41 +0000

    First Js Project

---
+++ Project/home.js
@@ -0,0 +1,28 @@
+
+ const button = document.querySelectorAll('.btn');
+ const body = document.querySelector('body');
+console.log(button);
+
+button.forEach(function (btn) {
+    btn.addEventListener('click',function (e){
+        console.log(e);
+        console.log(e.target);
+        
+        
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+         if (e.target.id === 'orange') {
+            body.style.backgroundColor = e.target.id;
+        }
+         if (e.target.id === 'green') {
+            body.style.backgroundColor = e.target.id;
+        } if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        } if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+
+    })
+    
+});
